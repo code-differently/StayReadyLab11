243e001a31bbc276a8436d78f5df3533317f9baf
Chastin Taylor <chastintaylor@gmail.com>
2020-07-23 15:28:29 -0400

    Finished Assignment, Ready for Review

--- src/test/java/SpellCheckerTest.java
+++ src/test/java/SpellCheckerTest.java
@@ -1,2 +1,21 @@
-public class SpellCheckerTest {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class SpellCheckerTest 
+{
+    public static void main(String[] args) throws FileNotFoundException
+    {
+        
+        SpellChecker spellChecker = new SpellChecker();
+        //To fill the HashSet object named dictionary with words
+        spellChecker.fillDict("words_alpha.txt");
+        
+        //To select a file by inputing into the terminal
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter the name of the file:");
+        String file = scan.next();
+        spellChecker.checkFile(file);
+
+    }
 }
