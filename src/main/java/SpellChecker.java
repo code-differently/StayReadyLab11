fca4353a83fe1e99916d386146c7b8eb48367ae5
Kaveesha <weerasirikaveesha@gmail.com>
2020-07-23 20:15:23 -0500

    finished part 1

--- src/main/java/SpellChecker.java
+++ src/main/java/SpellChecker.java
@@ -1,2 +1,78 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Scanner;
+import java.util.logging.Logger;
+
 public class SpellChecker {
+    Scanner scanner = new Scanner(System.in);
+    private final static Logger myLogger = Logger.getLogger("com.codedifferently.spellchecker");
+    private HashSet<String> setOfWords = new HashSet<String>();
+
+    public static void main(String[] args) {
+        SpellChecker checker = new SpellChecker();
+        checker.keepAskingUserUntilTheyInputTheRightFile();
+
+        myLogger.info("Size of the set: " + checker.getSetOfWords().size());
+        myLogger.info(checker.createListOfWordsFromSet());
+    }
+
+    private void keepAskingUserUntilTheyInputTheRightFile() {
+        String path;
+        boolean fileNotValid = true;
+        while(fileNotValid) {
+            try {
+                path = this.askUserForFilePath();
+                File file = this.tidyUpPathAndReturnFile(path);
+                this.readFile(file);
+                fileNotValid = false;
+            }
+            catch(FileNotFoundException fileNotFoundException) {
+                myLogger.severe("File cannot be found. Try again please.");
+            }
+        }
+    }
+
+    private String askUserForFilePath() {
+        Scanner scanner = new Scanner(System.in);
+
+        myLogger.info("Enter a file path name.");
+
+        return scanner.next() + "\n";
+    }
+
+    private File tidyUpPathAndReturnFile(String path) {
+        File file;
+        path = path.replace("\\", "\\\\");
+        //for some reason it was inserting a newline so I had to take it out
+        path = path.replace("\n", "");
+        file = new File(path);
+        return file;
+    }
+
+    public void readFile(File file) throws FileNotFoundException {
+        try (Scanner in = new Scanner(new FileInputStream(file), "UTF-8")) {
+            while(in.hasNext()) {
+                String individualWord = in.next().toLowerCase();
+                //replace a character that is not a-z with inserting nothing.
+                // (I don't check for A-Z because I change the string to lower case)
+                individualWord = individualWord.replaceAll("[^a-z]", "");
+                setOfWords.add(individualWord);
+            }
+        }
+    }
+
+    private String createListOfWordsFromSet() {
+        StringBuilder builder = new StringBuilder();
+        for (String word : setOfWords) {
+            builder.append("This is a word that is spelled correctly: " + word + "\n");
+        }
+        return builder.toString();
+    }
+
+    public HashSet<String> getSetOfWords() {
+        return this.setOfWords;
+    }
 }
--- src/test/java/SpellCheckerTest.java
+++ src/test/java/SpellCheckerTest.java
@@ -1,2 +1,34 @@
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.logging.Logger;
+
 public class SpellCheckerTest {
+    private File file;
+    private SpellChecker spellChecker;
+    private final static Logger myLogger = Logger.getLogger("com.codedifferently.spellchecker");;
+
+    @Before
+    public void setUp() {
+        file = new File("words_alpha.txt");
+        spellChecker = new SpellChecker();
+        try {
+            spellChecker.readFile(file);
+        }
+        catch(FileNotFoundException fileNotFound) {
+            myLogger.info("Try again");
+        }
+    }
+
+    @Test
+    public void getSetOfWordsTest() {
+        int expectedSize = 370104;
+
+        int actualSize = spellChecker.getSetOfWords().size();
+
+        Assert.assertEquals(expectedSize, actualSize);
+    }
 }
