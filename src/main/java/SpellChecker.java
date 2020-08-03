4b95915f21cbcb5219214ee109a2e9e587709d43
KeseanaH <45104070+KeseanaH@users.noreply.github.com>
2020-08-03 01:17:06 -0400

    still working, updates

--- src/main/java/SpellChecker.java
+++ src/main/java/SpellChecker.java
@@ -1,2 +1,134 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Scanner;
+import java.util.TreeSet;
+
 public class SpellChecker {
+    private HashSet<String> referenceSet;
+    private HashSet<String> set;
+    Scanner sc;
+
+    public void addReferenceFile(File file){
+        try{
+            referenceSet = new HashSet<String>();
+            sc = new Scanner(file);
+
+            while(sc.hasNextLine()){
+                String currentWord = sc.nextLine();
+                currentWord.toLowerCase();
+                referenceSet.add(currentWord);
+            }
+            sc.close();
+        }
+        catch(FileNotFoundException e){
+            System.out.println("The file was not found.");
+            e.printStackTrace();
+        }
+    }
+
+    public void addTextDoc(File file){
+        try{
+            set = new HashSet<String>();
+            sc = new Scanner(file);
+
+            while(sc.hasNextLine()){
+                String currentLine = sc.nextLine();
+                String[] words = currentLine.split(" ");
+
+                for(String w: words){
+                    w = w.toLowerCase().replaceAll("\\p{Punct}", "");
+                    set.add(w);
+                }
+            }
+            sc.close();
+        }
+        catch(FileNotFoundException e){
+            System.out.println("The file was not found.");
+            e.printStackTrace();
+        }
+    }
+
+    public boolean correctSpelling(String word){
+
+        for(String w: referenceSet){
+            if(w.equals(word))
+                return true;
+        }
+
+        return false;
+    }
+
+    public TreeSet<String> getSuggestedWords(String mispelledWord){
+        TreeSet<String> suggestedWords = new TreeSet<String>();
+        //StringBuilder mispelledWord = new StringBuilder(word);
+
+        StringBuilder suggestion1 = deleteOneLetter(mispelledWord); 
+        if(suggestion1 != null)
+            suggestedWords.add(suggestion1.toString());
+
+
+        return suggestedWords;
+    }
+
+    public StringBuilder deleteOneLetter(String w){
+        StringBuilder newWord = new StringBuilder();
+        StringBuilder word = new StringBuilder(w);
+
+        for(int i = 0; i < word.length(); i++){
+            word.deleteCharAt(i);
+
+            if(referenceSet.contains(word)) {
+                newWord = word;
+                return newWord;
+            }
+        }
+
+        return null;
+    }
+
+    public StringBuilder changeAnyLetter(String w){
+        StringBuilder newWord = new StringBuilder();
+        StringBuilder word = new StringBuilder(w);
+
+        for(int i = 0; i < word.length(); i++){
+            for(int j = 97; j <= 122; j++){
+
+                Character character = (char) j;
+                String ch = character.toString();
+                word.replace(i, i+1, ch);
+
+                if(referenceSet.contains(word)){
+                    newWord = word;
+                    return newWord;
+                }
+            }
+
+            word = new StringBuilder(w);
+        }
+        return null;
+    }
+
+    public void printSpellingReference(){
+        Iterator<String> it = referenceSet.iterator();
+        while(it.hasNext()){
+            System.out.println(it.next());
+        }
+    }
+
+    public void printTextDoc(){
+        Iterator<String> it = set.iterator();
+        while(it.hasNext()){
+            System.out.println(it.next());
+        }
+    }
+
+    protected HashSet<String> getSet(){
+        return set;
+    }
+
+    protected HashSet<String> getReferenceSet(){
+        return referenceSet;
+    }
 }
--- src/test/java/SpellCheckerTest.java
+++ src/test/java/SpellCheckerTest.java
@@ -1,2 +1,72 @@
+import java.io.File;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
 public class SpellCheckerTest {
+
+    SpellChecker sc;
+
+    @Before
+    public void initialize(){
+        sc = new SpellChecker();
+    }
+
+    @Test
+    public void addSpellingReferenceTest(){
+        File file = new File("words_alpha.txt");
+        sc.addReferenceFile(file);
+
+        Assert.assertFalse(sc.getReferenceSet().isEmpty());
+    }
+
+    @Test
+    public void addTextDocTest(){
+        File file = new File("letter_from_gandhi.txt");
+        sc.addTextDoc(file);
+
+        Assert.assertFalse(sc.getSet().isEmpty());
+    }
+
+    @Test
+    public void correctSpellingTestTrue(){
+        File file = new File("words_alpha.txt");
+        sc.addReferenceFile(file);
+
+        boolean actual = sc.correctSpelling("aahs");
+
+        Assert.assertTrue(actual);
+    }
+
+    @Test
+    public void correctSpellingTestFalse(){
+        File file = new File("words_alpha.txt");
+        sc.addReferenceFile(file);
+
+        boolean actual = sc.correctSpelling("aabs");
+
+        Assert.assertFalse(actual);
+    }
+
+    @Test
+    public void deleteOneLetterTest(){
+        File file = new File("words_alpha.txt");
+        sc.addReferenceFile(file);
+
+        StringBuilder actual = sc.deleteOneLetter("html");
+
+        Assert.assertNull(actual);
+    }
+
+    @Test
+    public void changeAnyLetterTest(){
+        File file = new File("words_alpha.txt");
+        sc.addReferenceFile(file);
+
+        StringBuilder actual = sc.changeAnyLetter("txt");
+        StringBuilder expected = new StringBuilder("tat");
+
+        Assert.assertEquals(expected, actual);
+    }
 }
